9461958c05fb7619e412a3f84f74482514928413
WeifengZhao1 <kweifeng16@gmail.com>
2021-10-01 06:14:37 -0400

    zip-code-react-lab

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div>
+      <div className="CityLocation">{props.CityLocation}</div>
+      <ul>
+        <li> State: {props.State}</li>
+        <li>Location: ({props.Lat},{props.Long})</li>
+        <li>Population: (estimated) {props.Population}</li>
+        <li>Total Wages: {props.TotalWages}</li>
+      </ul>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div className="zipcode">Zip Code:
+      <input type="text" placeholder="  Try 10016" onChange={props.changeHandler} />
+    </div>);
 }
 
-
 class App extends Component {
+  state = {
+    zipCode: '',
+    cities: [],
+  }
+
+  zipChanged = (event) => {
+    console.log(event.target.value);
+    this.setState({ zipCode: event.target.value })
+
+    if (this.state.cities.length === 0) {
+      console.log("No Result");
+    }
+    if (event.target.value.length === 5) {
+      fetch('http://ctp-zip-api.herokuapp.com/zip/' + event.target.value)
+        .then((res) => res.json())
+        .then((data) => {
+          this.setState({ cities: data })
+        })
+        .catch(error => {
+          this.setState({ cities: [] })
+        })
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.zipChanged}/>
         <div>
-          <City />
-          <City />
+          {
+            this.state.cities.map((city) =>
+              <City
+                State={city.State}
+                Population={city.EstimatedPopulation}
+                TotalWages={city.TotalWages}
+                Lat={city.Lat}
+                Long={city.Long}
+                CityLocation={city.LocationText}
+              />
+            )}
+             
+             <div className = "NoResult">
+            {
+              this.state.zipCode.length !== 5 && 
+              <h1>No Result</h1>
+            }
+           </div>
         </div>
       </div>
     );
